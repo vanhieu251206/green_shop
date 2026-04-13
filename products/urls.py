2a334edf6a7c45e820022212b57ef19af702a82f
vanhieu251206 <vanhieu25122006@gmail.com>
2026-04-14 01:11:10 +0700

    first commit

--- products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import product_list, product_detail
-
-urlpatterns = [
-    path('', product_list, name='product_list'),
-    path('<int:pk>/', product_detail, name='product_detail'),
-]
