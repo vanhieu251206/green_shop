af2c9e7d6667259afcc934d582090ccba92321e3
vanhieu251206 <vanhieu25122006@gmail.com>
2026-04-17 17:03:59 +0700

    Hoàn thiện giao diện cho trang sản phẩm

--- products/views.py
+++ products/views.py
@@ -6,12 +6,26 @@ def product_list(request):
     products = Product.objects.all()
 
     q = request.GET.get('q', '').strip()
+    sort = request.GET.get('sort', '').strip()
+
     if q:
         products = products.filter(name__icontains=q)
+
+    if sort == 'name_asc':
+        products = products.order_by('name')
+    elif sort == 'name_desc':
+        products = products.order_by('-name')
+    elif sort == 'price_asc':
+        products = products.order_by('price')
+    elif sort == 'price_desc':
+        products = products.order_by('-price')
+    else:
+        products = products.order_by('-id')
 
     return render(request, 'products/list.html', {
         'products': products,
         'q': q,
+        'sort': sort,
     })
 
 
