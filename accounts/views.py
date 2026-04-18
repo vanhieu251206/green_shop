675d756858ad76caeb8c3a92c70bece009523653
vanhieu251206 <vanhieu25122006@gmail.com>
2026-04-19 00:31:09 +0700

    Sửa dropdown tài khoản click để mở và khôi phục giao diện navbar

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm, LoginForm
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
 
 
 def register_view(request):
@@ -30,7 +32,12 @@ def login_view(request):
 
     return render(request, 'accounts/login.html', {'form': form})
 
-
+@require_POST
+@login_required(login_url='login')
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+@login_required(login_url='login')
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
