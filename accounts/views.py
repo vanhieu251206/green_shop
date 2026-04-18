98247a9b3852afb0f763a73ff5e1e89e7f4116ff
vanhieu251206 <vanhieu25122006@gmail.com>
2026-04-18 23:15:21 +0700

    Hoàn thiện trang đăng ký

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .forms import RegisterForm, LoginForm
 
 
 def register_view(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-
-    form = RegisterForm(request.POST or None)
-
-    if request.method == 'POST':
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('product_list')
+    else:
+        form = RegisterForm()
 
     return render(request, 'accounts/register.html', {'form': form})
 
